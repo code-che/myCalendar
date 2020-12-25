ac0c87c3b7d47ddc7c1d4bfc9cb396279b891ccf
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2020-12-26 00:47:18 +0330

    Google API calendar connected * .gitignore updated * Prettier configured

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,108 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect } from 'react';
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../styles/Home.module.css';
+import googleApiConfigs from '../googleApiConfigs';
 
-export default function Home() {
+let gapi = null;
+
+const handleClientLoad = () => {
+  console.log('handleClientLoad');
+  const script = document.createElement('script');
+  script.src = 'https://apis.google.com/js/api.js';
+  document.body.appendChild(script);
+  script.onload = () => {
+    window['gapi'].load('client:auth2', initClient);
+  };
+};
+
+const initClient = () => {
+  console.log('initClient');
+  gapi.client.init(googleApiConfigs).then(
+    () => {
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      // authorizeButton.onclick = handleAuthClick;
+      // signoutButton.onclick = handleSignoutClick;
+    },
+    (e) => console.log(e)
+  );
+};
+
+const updateSigninStatus = (isSignedIn) => {
+  console.log('updateSigninStatus');
+  if (isSignedIn) {
+    // authorizeButton.style.display = 'none';
+    // signoutButton.style.display = 'block';
+    listUpcomingEvents();
+  } else {
+    // authorizeButton.style.display = 'block';
+    // signoutButton.style.display = 'none';
+  }
+};
+
+const handleAuthClick = () => {
+  console.log('handleAuthClick');
+  gapi.auth2.getAuthInstance().signIn();
+};
+
+const handleSignoutClick = () => {
+  console.log('handleSignoutClick');
+  gapi.auth2.getAuthInstance().signOut();
+};
+
+const listUpcomingEvents = () => {
+  console.log('listUpcomingEvents');
+  gapi.client.calendar.events
+    .list({
+      calendarId: 'primary',
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: 'startTime',
+    })
+    .then((res) => {
+      const events = res.result.items;
+      console.log(events, '--- events ---');
+    });
+};
+
+const Home = () => {
+  useEffect(() => {
+    console.log('did mount');
+    gapi = window['gapi'];
+    handleClientLoad();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://apis.google.com/js/api.js" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="mb-2 text-monospace">Sign Status</div>
+      <div>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleAuthClick}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          Sign In
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary ml-2"
+          onClick={handleSignoutClick}
+        >
+          Sing Out
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
