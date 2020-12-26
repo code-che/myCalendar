8089f489a345c8b6d94ee195ab18e9f64185d9d2
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2020-12-26 07:13:54 +0330

    Login button UI changed

--- components/Login.js
+++ components/Login.js
@@ -8,12 +8,13 @@ const Login = () => {
       className={`${styles['sign-in-button']} d-flex align-items-center justify-content-between`}
       variant="success"
       onClick={handleAuthClick}
+      size="lg"
     >
       <img
         src="/google-glass-logo.svg"
         alt="Google"
-        width={20}
-        className="mr-3"
+        width={25}
+        className="mr-4"
       />
       Sign In
     </Button>
