e38b17270c92f8d2c67e845dda11c312d43861c5
codeChe <codeche@yahoo.com>
2021-06-24 22:12:01 +0430

    add script tag for google api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import {useEffect} from 'react';
+import Head from "next/head";
 import {connect} from 'react-redux';
 
 const Home = ({events, dispatch}) => {
 
     useEffect(() => {
-
+        
     }, []);
 
     return (
         <div>
+            <Head>
+                <title>CodeChe Calendar</title>
+                <script src="https://apis.google.com/js/client.js"></script>
+            </Head>
         </div>
     )
 };
